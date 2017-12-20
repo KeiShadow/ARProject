43b8e6bc1ca993164756d75e3e7c763399596cf2
Petr Noga <n.skypo@gmail.com>
2017-12-21 00:32:37 +0100

    Přidána About aktivita

---
+++ NativeCamera/app/src/main/java/com/keiko/nativecamera/Activity/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.keiko.nativecamera.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    Button
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
